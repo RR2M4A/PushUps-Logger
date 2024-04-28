8694c1f1f089ba7ec396d4b07d71db2e5e7abe1d
José Victor Gabriel <josevictor21112005@gmail.com>
2024-04-28 18:29:48 -0300

    #1 Login e Signup page: Aparência inicial da página

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def index():
+    return "Hello world"
+
+@main.route("/profile")
+def profile():
+    return "Profile Here"
